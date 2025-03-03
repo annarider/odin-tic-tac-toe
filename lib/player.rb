2563327e803767111049753b7132274c9e6c1fa7
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-03 11:29:36 -0700

    Add: Method to request user input
    Allow the player to choose the position to place
    a game piece symbol.

--- lib/player.rb
+++ lib/player.rb
@@ -6,4 +6,12 @@ class Player
     @symbol = symbol
   end
 
+  def choose
+    position = nil
+    until position.between?(0, 8)
+      puts "It's your turn. Choose your position between 0 and 8."
+      position = gets.chomp.to_i
+    end
+    position
+  end
 end
