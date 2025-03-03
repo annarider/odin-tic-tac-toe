f09731075048994a5fd626778d4a3793d681c059
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-03 09:44:31 -0700

    Add: Method to display the current board state
    This will be useful for the Game class later when
    it needs to display the board after every turn.
    It's also useful for testing.

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -9,6 +9,16 @@ class GameBoard
     board[position] = symbol
   end
 
+  def show_board
+    puts  <<~TEXT 
+      #{board[0]} | #{board[1]} | #{board[2]} 
+      ---------------------------
+      #{board[3]} | #{board[4]} | #{board[5]}
+      ---------------------------
+      #{board[6]} | #{board[7]} | #{board[8]}
+      TEXT
+  end
+
 end
 
 
