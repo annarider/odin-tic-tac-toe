3adfdd6840d195f19394ac1090bcb8bb44755719
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-03 09:34:23 -0700

    Add: Initialize board object and set positions
    First thing is to initialize the board object
    with a flat array that holds 9 elements. These
    represent each position in the board for Tic-Tac-Toe.

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,4 +1,21 @@
 class GameBoard
+attr_accessor :board
+
   def initialize
+    @board = Array.new(9, ' ')
   end
+
+  def set_symbol(position, symbol)
+    @board[position] = symbol
+  end
+
+
 end
+
+
+# board = ['O', 'O', 'O', ' ', 'X', ' ', ' ', ' ', 'X']
+board = GameBoard.new
+board.set_symbol(1, "X")
+board.test_full
+board.show_board
+puts board.full?
