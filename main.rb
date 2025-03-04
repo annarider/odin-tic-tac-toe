346a858aa2d4d74489533370e32e58c0c81bdfa8
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-04 11:43:18 -0700

    Cleanup: Remove messages for testing

--- main.rb
+++ main.rb
@@ -1,8 +1,4 @@
 require_relative 'lib/game'
 
 game = Game.new
-puts game.play_order
-puts game.player1.name, game.player1.symbol
-puts game.player2.name, game.player2.symbol
-puts game.current_player.name, game.current_player.symbol
 game.play
