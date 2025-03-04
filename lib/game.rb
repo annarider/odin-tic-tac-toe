93b0df3adfedea47ecc941e349daa3e0245520ee
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-03 21:12:16 -0700

    Add: Init method on instance variables
    Initialize the game object's instance variables,
    including the players with prompts to get the
    player's names. This method includes the important
    part of randomly assigning the symbols to each
    player and announcing the game play as if the
    Game class is a referee.
    
    To break up an already long initialize method,
    I decided to break off the logic for randomly
    deciding which player would go first.

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,24 @@
+require_relative 'game_board'
+require_relative 'player'
+
 class Game
+  attr_accessor :board, :player1, :player2, :current_player 
+
   def initialize
+    @board = GameBoard.new
+    puts "What's the first player's name?"
+    name1 = gets.chomp
+    symbols = ['X', 'O'].shuffle
+    @player1 = Player.new(name1, symbols[0])
+    puts "What's the second player's name?"
+    name2 = gets.chomp
+    @player2 = Player.new(name2, symbols[1])
+    @current_player = play_order
+    puts "#{current_player.name} is going first and is assigned #{current_player.symbol}."
   end
+
+  def play_order
+    [player1, player2].sample
+  end
+
 end
