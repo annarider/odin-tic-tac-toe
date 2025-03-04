13d8ad535c9bb44022a37883582c722e9d6de8d4
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-03 21:14:29 -0700

    Add: Testing statements to ensure Game init works

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
-require_relative 'game'
+require_relative 'lib/game'
 
 game = Game.new
-game.play
+puts game.play_order
+puts game.player1.name, game.player1.symbol
+puts game.player2.name, game.player2.symbol
+puts game.current_player.name, game.current_player.symbol
