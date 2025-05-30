d41ded5c464f62deab5489ae26307458385a9a51
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-05-30 12:26:51 -0600

    test: add test for partially full board

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -8,14 +8,36 @@ describe GameBoard do
   subject(:game_board) { described_class.new }
 
   describe '#full?' do
-    let(:full_board) { Array.new(9, 'X') }
-    context 'when every position on the board has a symbol' do
+    context 'when every position on the board has an X symbol' do
+      let(:full_board) { Array.new(9, 'X') }
       before do
         allow(game_board).to receive(:board).and_return(full_board)
       end
+
       it 'returns true' do
         expect(game_board.full?).to be true
       end
     end
+    context 'when every position on the board has an X or O symbol' do
+      let(:full_board) { Array.new(9, %w[X O].sample) }
+      before do
+        allow(game_board).to receive(:board).and_return(full_board)
+      end
+
+      it 'returns true' do
+        expect(game_board.full?).to be true
+      end
+    end
+    context 'when some positions on the board are empty' do
+      let(:part_full_board) { Array.new(9, %w[X O ' '].sample) }
+      before do
+        allow(game_board).to receive(:board).and_return(part_full_board)
+      end
+
+      it 'returns false' do
+        expect(game_board.full?).to be false
+      end
+    end
+
   end
 end
