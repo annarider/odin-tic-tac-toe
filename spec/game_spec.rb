58addb933044945cbe18d8eb77e4540feb8def1b
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-02 14:19:14 -0600

    test: add test for initializing game object

---
+++ spec/game_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../lib/game'
+
+# Tests for the Game class
+
+describe Game do
+  describe '#initialize' do
+    context 'when setting up a game' do
+      subject(:create_game) { described_class.new }
+      before do
+        allow_any_instance_of(Kernel).to receive(:gets).and_return("Anna\n", "Alex\n")
+        allow_any_instance_of(Game).to receive(:puts)
+      end
+
+      it "creates a new game object" do
+        expect(create_game).to respond_to(:play)
+      end
+    end
+  end
+
+end
